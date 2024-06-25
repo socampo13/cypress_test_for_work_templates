b35fae2966ab8ebeb11736172232ebe0e825d891
socampo13 <montioca16@gmail.com>
2024-06-25 11:10:37 -0500

    Refactor blog testing script to use constants for urls

--- cypress/e2e/blog_testing.cy.js
+++ cypress/e2e/blog_testing.cy.js
@@ -1,6 +1,14 @@
 //ALWAYS TAKE INTO ACCOUNT THE SLUGS TO AVOID ISSUES AND NOT WANTED ERRORS
 describe('Test Blog Listing templates functionalities', () => {
-    const baseUrl = 'https://45841846.hs-sites.com/blog-listing' //The baseUrl should be the URL from the LISTING page. Only change URL commented lines that explains it
+
+    const baseUrl = 'https://45763993.hs-sites.com/blog' //The baseUrl should be the URL from the LISTING page. Only change URL commented lines that explains it
+    const baseSearch = 'https://45763993.hs-sites.com/hs-search-results' //The baseSearch should be the URL from the SEARCH page. Only change URL commented lines that explains it
+    const basePage2 = 'https://45763993.hs-sites.com/blog/page/2' // only need to change https://45763993.hs-sites.com
+    const basePage3 = 'https://45763993.hs-sites.com/blog/page/3' // https://45763993.hs-sites.com
+    const basePost = 'https://45763993.fs1.hubspotusercontent-na1.net/hub/45841846/hubfs/Imported_Blog_Media/boost-website.jpg?width=500&name=boost-website.jpg' // Just change the URL comming from one of the post in the listing page
+    const basePostInside = 'https://45763993.hs-sites.com/blog/journey-continues' // URL from the post that was previously clicked
+    const nextPagePost = 'https://45763993.hs-sites.com/blog/shaping-web-content-future' // URL from the post that comes after clicking the "next article >" button
+    
     it('Search bar is working properly', () => {
         cy.visit(baseUrl);
 
@@ -9,19 +17,19 @@ describe('Test Blog Listing templates functionalities', () => {
         cy.get('button').click();
         cy.get('.hs-search-field__bar > form').submit();
 
-        cy.url().should('contains', 'https://45841846.hs-sites.com/hs-search-results'); //Only change the whole URL in here
+        cy.url().should('contains', baseSearch); 
    });
 
     it('Check if pagination is working correctly', () => {
         cy.visit(baseUrl);
         cy.get('.pagination__link--next').click();
-        cy.url().should('contains', 'https://45841846.hs-sites.com/blog-listing/page/2'); //Only change the whole URL in here
+        cy.url().should('contains', basePage2); 
         
         cy.get('.pagination__link--next').click();
-        cy.url().should('contains', 'https://45841846.hs-sites.com/blog-listing/page/3'); //Only change the whole URL in here
+        cy.url().should('contains', basePage3); 
         
         cy.get('.pagination__link--prev').click();
-        cy.url().should('contains', 'https://45841846.hs-sites.com/blog-listing/page/2'); //Only change the whole URL in here
+        cy.url().should('contains', basePage2); 
         
         cy.get('.pagination__link:nth-child(2)').click();
         cy.url().should('contains', baseUrl);
@@ -30,22 +38,22 @@ describe('Test Blog Listing templates functionalities', () => {
     it('Check if clicking a post redirects correctly', () => {
         cy.visit(baseUrl);
         
-        cy.get('.aos-init').first().find('.blog-index__post-content-image', 'https://45841846.fs1.hubspotusercontent-na1.net/hub/45841846/hubfs/Imported_Blog_Media/boost-website.jpg?width=500&name=boost-website.jpg').click(); //Only change the whole URL in here
-        cy.url().should('include', 'https://45841846.hs-sites.com/blog-listing/journey-continues'); //Only change the whole URL in here
+        cy.get('.aos-init').first().find('.blog-index__post-content-image', basePost).click(); 
+        cy.url().should('include', basePostInside); 
    });
 
     it('Buttons inside the posts are working correctly', () => {
-        cy.visit('https://45841846.hs-sites.com/blog-listing/journey-continues'); //Only change the whole URL in here
+        cy.visit(basePostInside); 
 
         cy.get('.button:nth-child(2)').click();
-        cy.url().should('contains', 'https://45841846.hs-sites.com/blog-listing/shaping-web-content-future'); //Only change the whole URL in here
+        cy.url().should('contains', nextPagePost); 
         
         cy.get('.blog-post__navigation > .button').click();
         cy.url().should('contains', baseUrl); 
    });
 
     it('Check if the form inside posts works correctly', () => {
-        cy.visit('https://45841846.hs-sites.com/blog-listing/journey-continues'); //Only change the whole URL in here
+        cy.visit(basePostInside); 
 
         cy.get('.input').click();
         cy.get('.input').type('support@onthefuze.com');
