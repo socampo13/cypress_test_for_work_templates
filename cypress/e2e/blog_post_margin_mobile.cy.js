06861363de8e685824eccde81673a0b66d054ff2
socampo13 <montioca16@gmail.com>
2024-06-25 16:33:12 -0500

    New tests for tablet and mobile view in the blog posts

---
+++ cypress/e2e/blog_post_margin_mobile.cy.js
@@ -0,0 +1,19 @@
+describe('Check if the template is having padding issues in tablet and smartphone size', () => {
+    const baseUrl = 'https://45763993.hs-sites.com/blog/shaping-web-content-future'
+    beforeEach(() => {
+     cy.viewport(453, 816);
+    });
+    it('Go to Related posts in tablet size and verify padding', () => {
+       
+    cy.visit(baseUrl);
+     
+    cy.get('.blog-related').within(() => {
+        cy.get('.blog-related__grid').then((cards) => {
+            cy.wrap(cards).first().should('have.css', 'padding', '0px');
+            cy.wrap(cards).first().should('have.css', 'padding', '0px');
+        });
+    }); 
+    cy.get('.blog-related__content').should('have.css', 'padding', '32px');
+    cy.get('.blog-related__content').should('have.css', 'padding', '32px');
+    });
+});
