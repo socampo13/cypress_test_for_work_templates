4b6540ca0094e9315cf3f16b435695c27bcc2ece
socampo13 <montioca16@gmail.com>
2024-06-28 08:24:38 -0500

    formatting

--- cypress/e2e/calling_an_API.cy.js
+++ cypress/e2e/calling_an_API.cy.js
@@ -1,45 +1,45 @@
-describe('Asynchronous testing with the PokeAPI', () => {
-   it('Should get info from a Pokemon and check its result', () => {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+describe("Asynchronous testing with the PokeAPI", () => {
+  it("Should get info from a Pokemon and check its result", () => {
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
     cy.request(apiUrl).then((response) => {
+      expect(response.status).to.eq(200);
+
+      expect(response.body).to.have.property("name", "pikachu");
+      expect(response.body).to.have.property("id", 25);
+      expect(response.body).to.have.property("base_experience");
+      expect(response.body).to.have.property("height");
+      expect(response.body).to.have.property("weight");
+      expect(response.body).to.have.property("types");
+    });
+  });
+
+  it("Should get information from multiple Pokemons and its abilities", () => {
+    const pokemonList = ["pikachu", "bulbasaur", "charmander"];
+
+    pokemonList.forEach((pokemon) => {
+      const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+      cy.request(apiUrl).then((response) => {
         expect(response.status).to.eq(200);
 
-        expect(response.body).to.have.property('name', 'pikachu');
-        expect(response.body).to.have.property('id', 25);
-        expect(response.body).to.have.property('base_experience');
-        expect(response.body).to.have.property('height');
-        expect(response.body).to.have.property('weight');
-        expect(response.body).to.have.property('types');
-    });
-   });
+        expect(response.body).to.have.property("name", pokemon);
+        expect(response.body).to.have.property("id");
+        expect(response.body).to.have.property("base_experience");
+        expect(response.body).to.have.property("height");
+        expect(response.body).to.have.property("weight");
+        expect(response.body).to.have.property("types");
+        expect(response.body).to.have.property("abilities");
 
-   it('Should get information from multiple Pokemons and its abilities', () => {
-      const pokemonList = ['pikachu', 'bulbasaur', 'charmander'];
+        const abilityUrl = response.body.abilities[0].ability.url;
 
-      pokemonList.forEach((pokemon) => {
-        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+        cy.request(abilityUrl).then((abilityResponse) => {
+          expect(abilityResponse.status).to.eq(200);
 
-        cy.request(apiUrl).then((response) => {
-            expect(response.status).to.eq(200);
-
-            expect(response.body).to.have.property('name', pokemon);
-            expect(response.body).to.have.property('id');
-            expect(response.body).to.have.property('base_experience');
-            expect(response.body).to.have.property('height');
-            expect(response.body).to.have.property('weight');
-            expect(response.body).to.have.property('types');
-            expect(response.body).to.have.property('abilities');
-
-            const abilityUrl = response.body.abilities[0].ability.url;
-
-            cy.request(abilityUrl).then((abilityResponse) => {
-                expect(abilityResponse.status).to.eq(200);
-
-                expect(abilityResponse.body).to.have.property('name');
-                expect(abilityResponse.body).to.have.property('effect_entries');
-            });
+          expect(abilityResponse.body).to.have.property("name");
+          expect(abilityResponse.body).to.have.property("effect_entries");
         });
       });
-   });
+    });
+  });
 });
--- cypress/e2e/test_for_object_oriented_code.cy.js
+++ cypress/e2e/test_for_object_oriented_code.cy.js
@@ -1,43 +1,43 @@
-const MyClass = require('../javascript-code/objects');
+const MyClass = require("../javascript-code/objects");
 
-describe('Testing object oriented code', () => {
-   let instance;
+describe("Testing object oriented code", () => {
+  let instance;
 
-   beforeEach(() => {
-      instance = new MyClass('Alice', 25);
-   });
+  beforeEach(() => {
+    instance = new MyClass("Alice", 25);
+  });
 
-   it('should create an instance of MyClass', () => {
-      expect(instance).to.be.an('object');
-      expect(instance.name).to.equal('Alice');
-      expect(instance.age).to.equal(25);
-   });
+  it("should create an instance of MyClass", () => {
+    expect(instance).to.be.an("object");
+    expect(instance.name).to.equal("Alice");
+    expect(instance.age).to.equal(25);
+  });
 
-   it('Should greet properly', () => {
-      const greeting = instance.greet();
-      expect(greeting).to.equal('Hello, Alice!');
-   });
+  it("Should greet properly", () => {
+    const greeting = instance.greet();
+    expect(greeting).to.equal("Hello, Alice!");
+  });
 
-   it('Should increment age on birthday', () => {
-      const newAge = instance.haveBirthday();
-      expect(newAge).to.equal(26);
-      expect(instance.age).to.equal(26);
-   });
+  it("Should increment age on birthday", () => {
+    const newAge = instance.haveBirthday();
+    expect(newAge).to.equal(26);
+    expect(instance.age).to.equal(26);
+  });
 
-   it('Should add a friend', () => {
-      instance.addFriend('Bob');
-      expect(instance.getFriends()).to.include('Bob');
-   });
+  it("Should add a friend", () => {
+    instance.addFriend("Bob");
+    expect(instance.getFriends()).to.include("Bob");
+  });
 
-   it('Should return all friends', () => {
-      instance.addFriend('Bob');
-      instance.addFriend('Charlie');
-      expect(instance.getFriends()).to.deep.equal(['Bob', 'Charlie']);
-   });
+  it("Should return all friends", () => {
+    instance.addFriend("Bob");
+    instance.addFriend("Charlie");
+    expect(instance.getFriends()).to.deep.equal(["Bob", "Charlie"]);
+  });
 
-   it('Should determine if the person is an adult', () => {
-      expect(instance.isAdult()).to.be.true;
-      instance = new MyClass('Dave', 17);
-      expect(instance.isAdult()).to.be.false;
-   });
+  it("Should determine if the person is an adult", () => {
+    expect(instance.isAdult()).to.be.true;
+    instance = new MyClass("Dave", 17);
+    expect(instance.isAdult()).to.be.false;
+  });
 });
