16bd1965e6a9af432eed690f5da7e404df56c7aa
socampo13 <montioca16@gmail.com>
2024-07-03 07:31:12 -0500

    Real tests that worked!!!

--- cypress/e2e/template1_form.cy.js
+++ cypress/e2e/template1_form.cy.js
@@ -1,7 +1,7 @@
 describe('Form submission test', () => {
     it('Should fill out and submit the form', () => {
       // Visit the page with the form
-      cy.visit('https://44909384.hs-sites.com/landing-page'); // Replace with the actual URL of your form
+      cy.visit('https://45594336.hs-sites.com/landing-page'); // Replace with the actual URL of your form
   
       // Fill out the form fields using different types of selectors
       cy.get('input[Name="firstname"]').type('John'); // Using the 'name' attribute
