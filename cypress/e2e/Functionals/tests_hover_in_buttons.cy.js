508b6e2177bbdb5197458c3486ac4c890d9f26f7
socampo13 <montioca16@gmail.com>
2024-07-18 14:47:09 -0500

    Add comments

--- cypress/e2e/Functionals/tests_hover_in_buttons.cy.js
+++ cypress/e2e/Functionals/tests_hover_in_buttons.cy.js
@@ -4,25 +4,27 @@ describe('Checks hover effects in buttons', () => {
    });
 
    it('Hovers are working', () => {
+// Save the original styles. NOT NEEDED TO ADD ANY COLOR
       const originalBgColors = [];
       const originalColors = [];
       const originalBorderColors = [];
-
+// Find all elements of buttons and iterate over them
       cy.get('button').each(($btn, index) => {
+// Saves the original background-color 
         cy.wrap($btn).invoke('css', 'background-color').then((bgColor) => {
             originalBgColors[index] = bgColor;
         });
-
+// Saves the origianl text color
         cy.wrap($btn).invoke('css', 'color').then((color) => {
             originalColors[index] = color;
         });
-
+// Saves the original border-color
         cy.wrap($btn).invoke('css', 'border-color').then((borderColor) => {
             originalBorderColors[index] = borderColor;
         });
-
+// Triggers the hover state, simulates the mouse getting over the buttons
         cy.wrap($btn).trigger('mouseover', { force: true });
-
+// Checks if styles have changed
         cy.wrap($btn).invoke('css', 'background-color').then((hoverBgColor) => {
             if (hoverBgColor === originalBgColors[index]){
                 cy.log(`Button ${index + 1} does not have a hover effect on background-color`);
@@ -40,7 +42,7 @@ describe('Checks hover effects in buttons', () => {
                 cy.log(`Button ${index + 1} does not have a hover effect on border-color`);
             }
         });
-
+// Simulates hover state finishes by moving the mouse away from the button
         cy.wrap($btn).trigger('mouseout', { force: true });
       });
    });
