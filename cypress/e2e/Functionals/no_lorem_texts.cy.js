c0c2c9dac4fd0808af25e4e71556d27131c3c11d
socampo13 <montioca16@gmail.com>
2024-07-22 13:36:55 -0500

    change name and delete duplicate

--- cypress/e2e/Functionals/no_lorem_texts.cy.js
+++ cypress/e2e/Functionals/no_lorem_texts.cy.js
@@ -1,27 +1,36 @@
-describe("In the content of the page, there shouldnt be lorem ipsum texts", () => {
-  const baseUrl = "https://12br.com";
+describe("Checks for lorem ipsum texts while visiting all pages", () => {
+  const urls = [
+    "https://12br.com",
+    "https://12br.com/about-us",
+    "https://12br.com/our-services",
+    "https://12br.com/learning-center",
+    "https://12br.com/careers-page",
+    "https://12br.com/sales",
+    "https://12br.com/blog",
+    "https://12br.com/contact-us",
+  ];
+
   beforeEach(() => {
     cy.on("uncaught:exception", (err, runnable) => {
       return false;
     });
   });
 
-  it("No lorem ipsum in the body of the page ", () => {
-    cy.visit(baseUrl);
-    cy.get("body").then(($body) => {
-      const bodyText = $body.text().toLowerCase();
-      expect(bodyText).to.not.include("lorem ipsum");
-    });
-  });
+  urls.forEach((url) => {
+    it(`should not contain 'Lorem Ipsum' text on ${url}`, () => {
+      cy.visit(url);
 
-  it("Double checks in headings, paragraphs, links and other elements", () => {
-    cy.visit(baseUrl);
-    const elementsToCheck = ["p", "a", "h1", "h2", "h3", "h4", "h5", "h6"];
+      const elementsToCheck = ["p", "a", "h1", "h2", "h3", "h4", "h5", "h6"];
 
-    elementsToCheck.forEach((selector) => {
-      cy.get(selector).each(($el) => {
-        const text = $el.text();
-        expect(text).to.not.include("Lorem Ipsum");
+      elementsToCheck.forEach((selector) => {
+        cy.get("body").then(($body) => {
+          if ($body.find(selector).length > 0) {
+            cy.get(selector).each(($el) => {
+              const text = $el.text();
+              expect(text).to.not.include("Lorem Ipsum");
+            });
+          }
+        });
       });
     });
   });
