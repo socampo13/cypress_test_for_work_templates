d50a7d0c645ee53944f70e3563f99d28cbef2bf0
socampo13 <montioca16@gmail.com>
2024-07-24 08:34:24 -0500

    add uncaught exception

--- cypress/e2e/Functionals/page_loading_time.cy.js
+++ cypress/e2e/Functionals/page_loading_time.cy.js
@@ -1,10 +1,16 @@
 Cypress.config('pageLoadTimeout', 30000);
 
+beforeEach(() => {
+  cy.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
+});
+
 describe('Page Load Time Test', () => {
     it('should measure page load time and assert it is less than 10 seconds', () => {
       const startTime = Date.now();
   
-      cy.visit('https://www.evettfield.com/terms-of-use/');
+      cy.visit('https://12br.com');
   
       cy.window().then(() => {
         const endTime = Date.now();
